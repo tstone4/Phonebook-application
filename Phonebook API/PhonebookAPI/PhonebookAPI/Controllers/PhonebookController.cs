b33e7b38622a48cae3fe44ec55dd6e3f101332b5
Thomas Stone <tstone@epitec.com>
2019-10-06 19:52:12 -0400

    setup and is now functioning with an sql database

--- Phonebook API/PhonebookAPI/PhonebookAPI/Controllers/PhonebookController.cs
+++ Phonebook API/PhonebookAPI/PhonebookAPI/Controllers/PhonebookController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
+using Dapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using PhonebookAPI.Models;
@@ -14,35 +16,12 @@ namespace PhonebookAPI.Controllers
         [Route("phonebook")]
         public ActionResult<PhonebookEntry[]> Get()
         {
-            PhonebookEntry entry1 = new PhonebookEntry();
-            entry1.FirstName = "Thomas";
-            entry1.LastName = "Stone";
-            entry1.Id = 1;
-            entry1.PhoneNumber = "1111111111";
-
-            PhonebookEntry entry2 = new PhonebookEntry();
-            entry2.FirstName = "Ryan";
-            entry2.LastName = "Mcnichol";
-            entry2.Id = 2;
-            entry2.PhoneNumber = "222222222";
-
-            PhonebookEntry entry3 = new PhonebookEntry();
-            entry3.FirstName = "Aaron";
-            entry3.LastName = "Ziaja";
-            entry3.Id = 3;
-            entry3.PhoneNumber = "3333333333";
-            
-            
-            
-            PhonebookEntry[] array = new PhonebookEntry[3];
-            array[0] = entry1;
-            array[1] = entry2;
-            array[2] = entry3;
-
-
-
-
-            return array;
+            using (var context = new SqlConnection("Server=localhost;Database=PhonebookTestDb;Trusted_Connection=True;"))
+            {
+                var sql = @"SELECT Id, FirstName, LastName, PhoneNumber FROM [Entry]";
+                var results = context.Query<PhonebookEntry>(sql).ToArray();
+                return results;
+            }
         }
     }
 }
